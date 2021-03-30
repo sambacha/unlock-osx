b88b45c5dcdd4d5a889f6cbd34a045aef3154363
sam bacha <sam@freighttrust.com>
2021-03-30 07:18:46 -0700

    nano.sh

---
+++ Install/src/nano.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# osx comes with nano 2.0.1
+# latest nano version is 5.6.1
+wget https://www.nano-editor.org/dist/v5/nano-5.6.1.tar.gz
+tar -xf nano-5.6.1.tar.gz
+rm nano-5.6.1.tar.gz
+cd nano-5.6.1
+CONFIG_SHELL=/bin/bash ./configure CONFIG_SHELL=/bin/bash
+make
+sudo make install
