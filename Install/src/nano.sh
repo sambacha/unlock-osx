e8fbc29c3129643ecd13138279c0e702c1587a78
sam bacha <sam@freighttrust.com>
2021-03-30 07:38:56 -0700

    /usr/bin/nano

--- Install/src/nano.sh
+++ Install/src/nano.sh
@@ -4,7 +4,7 @@
 wget https://www.nano-editor.org/dist/v5/nano-5.6.1.tar.gz
 tar -xf nano-5.6.1.tar.gz
 rm nano-5.6.1.tar.gz
+rm -rf /usr/bin/nano
 cd nano-5.6.1
-CONFIG_SHELL=/bin/bash ./configure CONFIG_SHELL=/bin/bash
-make
-sudo make install
+CONFIG_SHELL=/bin/bash ./configure CONFIG_SHELL=/bin/bash && make && sudo make install
+sudo mv /usr/local/bin/nano /usr/bin/nano
