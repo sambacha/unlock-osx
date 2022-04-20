89cbea5969219d6c5bc44e327ae91ba9b180946b
sam bacha <sam@manifoldfinance.com>
2022-04-20 07:20:47 -0700

    fix autoconf setup

--- Install/gnu/autoconf.sh
+++ Install/gnu/autoconf.sh
@@ -1,6 +1,8 @@
-http://mirror.us-midwest-1.nexcess.net/gnu/autoconf/autoconf-latest.tar.gz
-tar autoconf-latest.tar.gz
-cd autoconf-2.69
+#!/bin/sh
+wget http://mirror.us-midwest-1.nexcess.net/gnu/autoconf/autoconf-2.71.tar.xz
+tar -xf autoconf-latest.tar.gz
+cd autoconf-latest/
+cd autoconf-2.71
 chmod -R u+w $(whoami) /usr/local
 chmod -R u+w /usr/local
-CONFIG_SHELL=/bin/bash ./configure CONFIG_SHELL=/bin/bash
+CONFIG_SHELL=/usr/bin/env bash ./configure CONFIG_SHELL=/usr/bin/env bash
