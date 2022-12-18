34e9f13370a400a44d9767b004898c5d03ea2b67
sam bacha <sam@manifoldfinance.com>
2022-12-17 19:31:26 -0800

    fix bashism

--- Install/configure_gnu.sh
+++ Install/configure_gnu.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-CONFIG_SHELL=/bin/bash ./configure CONFIG_SHELL=/bin/bash && make && sudo make install || {
-CONFIG_SHELL=/usr/env/bin bash ./configure --prefix=$HOME
-}
+# CONFIG_SHELL=/usr/bin/env bash ./configure --prefix=$HOME
+CONFIG_SHELL=/bin/bash ./configure CONFIG_SHELL=/bin/bash && make && sudo make install || { CONFIG_SHELL=/usr/bin/env bash ./configure --prefix=$HOME; }
